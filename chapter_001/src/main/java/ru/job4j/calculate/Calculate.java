bb35d0de1454b33cd8adbe1bdaa9ec35ac9191a7
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-06-09 19:39:18 +0300

    1.3. JavaDoc. Code convention[#147276]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* @author – Lena Kachanova (konyakova.elena@gmail.com)
+* @since – 09.06.2019
+*/
+
 public class Calculate {
+	
+/**
+* Конструктор, вывод строки в консоль.
+* @param arg - arg.
+*/
+
 public static void main (String[] args){
 	System.out.println("Hello World");
 	}	
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,10 @@
+ 
+
+/**
+* Package for calculate task.
+*
+* @author Lena Kachanova (konyakova.elena@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
