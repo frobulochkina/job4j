99635b278ea103cd6178b5ed9b875c44383df8c7
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-06-10 20:13:50 +0300

    1.5. Checkstyle[#147278]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -20,7 +20,7 @@ public class Calculate {
 * @param args - args.
 */
 
-public static void main (String[] args){
+public static void main(String[] args) {
 	System.out.println("Hello World");
 	}	
 }
