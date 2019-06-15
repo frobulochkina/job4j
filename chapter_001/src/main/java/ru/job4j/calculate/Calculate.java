dc6deda0d633317fafb4d127c5c0a8cdc87773b7
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-06-15 22:42:52 +0300

    add .gitignore .idea *.iml

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,26 +1,26 @@
 package ru.job4j.calculate;
 
 /**
-* @author – Lena Kachanova (konyakova.elena@gmail.com)
-* @since – 09.06.2019
-*/
+ * @author – Lena Kachanova (konyakova.elena@gmail.com)
+ * @since – 09.06.2019
+ */
 
 public class Calculate {
-	/**
- * Method echo.
- * @param name Your name.
- * @return Echo plus your name.
- */
- public String echo(String name) {
-    return "Echo, echo, echo : " + name;
- }
-	
-/**
-* Конструктор, вывод строки в консоль.
-* @param args - args.
-*/
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 
-public static void main(String[] args) {
-	System.out.println("Hello World");
-	}	
+    /**
+     * Конструктор, вывод строки в консоль.
+     * @param args - args.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
