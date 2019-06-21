b1e6f3ba99bb2668c23d7f8eb05fbb216c719349
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-06-21 22:36:53 +0300

    class Fit

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,35 @@
 package ru.job4j.calculator;
 
-public class Calculator {
+/**
+ * @author – Lena Kachanova (konyakova.elena@gmail.com)
+ * @since – 18.06.2019
+ */
 
+public class Calculator {
+    /**
+     * Калькулятор, функция сложения.
+     */
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     * Калькулятор, функция вычитания.
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
 
+    /**
+     * Калькулятор, функция деления.
+     */
     public double div(double first, double second) {
         return first / second;
     }
 
+    /**
+     * Калькулятор, функция умножения.
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
