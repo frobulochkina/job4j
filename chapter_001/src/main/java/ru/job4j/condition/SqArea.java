4070500092a9fb06ab18185a7d6e167dff2f1653
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-06-24 20:38:55 +0300

    SqAreaTest.java

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+/**
+ * Программа вычисления сторон и площади прямоугольника
+ */
+
+public class SqArea {
+    /**
+     * @param k коэффициент.
+     * @param p периметр.
+     * @return площадь.
+     */
+    public int square(int p, int k) {
+        int h = p / 2 / 3;
+        int w = h * k;
+        return w * h;
+    }
+}
