f480c8c22bea1a0c193eba97ee4cbebfaec2dfa0
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-07-06 19:03:12 +0300

    MultiMax_.java

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first, int second, int third) {
-        return ((first > second) && (first > third)) ? first
-                : ((second > first) && (second > third)) ? second
-                : third;
+        int a = (first > second) ? first : second;
+        return a > third ? a : third;
     }
 }
