35a30b68a03c9cfc43961e160a2542d4d00abb2c
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-08-12 20:22:12 +0300

    FindLoop___.java

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -9,7 +9,7 @@ public class FindLoop {
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         int index;
-        for (index = 0; index <= el; index++) {
+        for (index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
