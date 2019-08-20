80ade5587b1bbdea95249fe147fa536066189f31
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-08-20 19:46:28 +0300

    ArrayChar_.java

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -18,6 +18,7 @@ public class ArrayChar {
         for (int i = 0; i < pref.length; i++){
                 if (pref[i] != wrd[i]) {
                     result = false;
+                    break;
                 }
         }
         // проверить. что массив data имеет первые элементы одинаковые с value
