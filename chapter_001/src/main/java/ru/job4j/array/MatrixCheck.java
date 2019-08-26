993de8ac5dcb4112660d5f3e39210224b504092b
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-08-26 20:10:06 +0300

    MatrixCheck.java

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,7 +11,7 @@ public class MatrixCheck {
         for (int i = 0; i < data.length - 1; i++) {
             if ((data[i][i] != data[i + 1][i + 1]) || (data[i][data.length - 1] != data[i + 1][data.length - 1 - 1]))  {
                 return false;
-        } else if ((data[i][i] == data[i + 1][i + 1]) && (data[i][data.length - 1] == data[i + 1][data.length - 1 - 1])) {
+            } else if ((data[i][i] == data[i + 1][i + 1]) && (data[i][data.length - 1] == data[i + 1][data.length - 1 - 1])) {
                 return true;
 
             }
