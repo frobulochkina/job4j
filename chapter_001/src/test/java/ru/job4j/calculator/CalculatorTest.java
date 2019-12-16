5c5d3dd3de88ada6b9a320cf2efb1da42ae4dec5
Frosya Bulochkina <konyakova.elena@gmail.com>
2019-12-16 22:08:09 +0300

    3. Перегрузить метод max для трех чисел [#147351]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -15,6 +15,22 @@ public class CalculatorTest<is> {
     }
 
     @Test
+    public void whenAddOnePlusOnePlusOneThenThree() {
+        Calculator calc = new Calculator();
+        double result = calc.add(1D, 1D, 1D);
+        double expected = 3D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenAddOnePlusOnePlusOnePlusOneThenFour() {
+        Calculator calc = new Calculator();
+        double result = calc.add(1D, 1D, 1D, 1D);
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
     public void whenAddFiveSubtractFourThenOne() {
         Calculator calc = new Calculator();
         double result = calc.subtract(5D, 4D);
