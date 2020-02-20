38aafa10ede082331941ce4a7c7606233fcf6c94
Frosya Bulochkina <konyakova.elena@gmail.com>
2020-02-20 20:45:58 +0300

    3. Перегрузить метод max для трех чисел [#147351]_

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void when3And5Then3() {
+        Point first = new Point(3, 3,0);
+        Point second = new Point(5, 5, 1);
+        double result = first.distance3d(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result = %s", result));
+        assertThat(result, is(3.0));
+    }
+
+}
